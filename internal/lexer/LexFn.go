66b926f2c1d74ed95fb4c03875fa91b6b9de0fb2
Karocyt/kevazoul <azoulay.kevin+robot@gmail.com>
2019-10-27 14:59:32 +0100

    lexer replaced by this in functions declarations

--- internal/lexer/LexFn.go
+++ internal/lexer/LexFn.go
@@ -10,7 +10,7 @@ type LexFn func(*Lexer) LexFn
 This lexer function starts everything off. It determines if we are
 beginning with a bracket, a key or facts.
 */
-func LexBegin(lexer *Lexer) LexFn {
+func LexBegin(this *Lexer) LexFn {
   this.SkipWhitespace()
 
   if strings.HasPrefix(this.InputToEnd(), LEFT_BRACKET) {
@@ -24,13 +24,13 @@ func LexBegin(lexer *Lexer) LexFn {
   }
 }
 
-func LexQuery(lexer *Lexer) LexFn {
+func LexQuery(this *Lexer) LexFn {
 	this.Next()
 	this.Emit(TOKEN_QUERY)
 	return LexKeyQuery
 }
 
-func LexKeyQuery(lexer *Lexer) LexFn {
+func LexKeyQuery(this *Lexer) LexFn {
 	this.Next()
 	if strings.ContainsRune(KEYS, this.Input[this.Start]) {
 		this.Emit(TOKEN_KEY)
@@ -45,14 +45,14 @@ func LexKeyQuery(lexer *Lexer) LexFn {
 This lexer function emits a TOKEN_LEFT_BRACKET then returns
 the lexer for a key.
 */
-func LexLeftBracket(lexer *Lexer) LexFn {
+func LexLeftBracket(this *Lexer) LexFn {
   this.Pos += len(LEFT_BRACKET)
   this.BracketCount += 1
   this.Emit(TOKEN_LEFT_BRACKET)
   return LexKey
 }
 
-func LexRightBracket(lexer *Lexer) LexFn {
+func LexRightBracket(this *Lexer) LexFn {
   this.Pos += len(RIGHT_BRACKET)
   this.BracketCount -= 1
   if this.BracketCount < 0 {
@@ -62,7 +62,7 @@ func LexRightBracket(lexer *Lexer) LexFn {
   return LexSymbol
 }
 
-func LexKey(lexer *Lexer) LexFn {
+func LexKey(this *Lexer) LexFn {
 	if strings.ContainsRune(KEYS, this.Input[this.Pos]) {
 		this.Next()
 		this.Emit(TOKEN_KEY)
@@ -74,7 +74,7 @@ func LexKey(lexer *Lexer) LexFn {
     }
 }
 
-func LexSymbol(lexer *Lexer) LexFn {
+func LexSymbol(this *Lexer) LexFn {
 	if strings.HasPrefix(this.InputToEnd(), IMPLIES) {
     	return LexImplies
     } else if strings.HasPrefix(this.InputToEnd(), IF_ONLY_IF) {
@@ -84,19 +84,19 @@ func LexSymbol(lexer *Lexer) LexFn {
     }
 }
 
-func LexImplies(lexer *Lexer) LexFn {
+func LexImplies(this *Lexer) LexFn {
 	this.Pos += len(IMPLIES)
-	this.Emit(lexertoker.TOKEN_IMPLIES)
+	this.Emit(thistoker.TOKEN_IMPLIES)
 	return LexResult
 }
 
-func LexIfOnlyIf(lexer *Lexer) LexFn {
+func LexIfOnlyIf(this *Lexer) LexFn {
 	this.Pos += len(IF_ONLY_IF)
-	this.Emit(lexertoker.TOKEN_IF_ONLY_IF)
+	this.Emit(thistoker.TOKEN_IF_ONLY_IF)
 	return LexResult
 }
 
-func LexOperator(lexer *Lexer) LexFn {
+func LexOperator(this *Lexer) LexFn {
 	this.Next()
 	if strings.HasPrefix(OPERATORS, this.Input[this.Start]) {
 		this.Emit(TOKEN_OPERATOR)
@@ -105,7 +105,7 @@ func LexOperator(lexer *Lexer) LexFn {
 	return LexError
 }
 
-func LexResult(lexer *Lexer) LexFn {
+func LexResult(this *Lexer) LexFn {
 	this.Next()
 	if strings.ContainsRune(KEYS, this.Input[this.Start]) {
 		this.Emit(TOKEN_KEY)
@@ -128,13 +128,13 @@ func LexResult(lexer *Lexer) LexFn {
 	return LexError
 }
 
-func LexEquals(lexer *Lexer) LexFn {
+func LexEquals(this *Lexer) LexFn {
   	this.Pos += len(EQUALS)
   	this.Emit(TOKEN_EQUALS)
   	return LexFact
 }
 
-func LexFact(lexer *Lexer) LexFn {
+func LexFact(this *Lexer) LexFn {
 	if strings.ContainsRune(KEYS, this.Input[this.Pos]) {
 		this.Pos += 1
 		this.Emit(TOKEN_KEY)
