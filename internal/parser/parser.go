b149e4a3df3f33c2c18f23777c4a94b3d6e759a9
Karocyt/kevazoul <azoulay.kevin+robot@gmail.com>
2020-01-04 00:13:04 +0100

    'process_line' function created in parser

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -9,6 +9,11 @@ import (
 const MAX_ITEMS = 50
 
 
+func process_line(a []lexer.LexToken) {
+	fmt.Println(a)
+	fmt.Println("\n")
+}
+
 func Parse(input string, filename string) (count int, e error) {
 	l := lexer.BeginLexing(input, filename)
 	a := make([]lexer.LexToken, 0, 50)
@@ -17,11 +22,12 @@ func Parse(input string, filename string) (count int, e error) {
 			break
 		}
 		if t.Type == lexer.TOKEN_EOL {
-			a = make([]lexer.LexToken, MAX_ITEMS)
-			fmt.Println("\n") ///////////////////////////////// DEBUG
+			if len(a) > 0 {
+				process_line(a)
+			}
+			a = make([]lexer.LexToken, 0, MAX_ITEMS)
 		} else if t.Type != lexer.TOKEN_EOF {
 			a = append(a, t)
-			fmt.Println(a[len(a)-1]) ////////////////////////// DEBUG
 			count++
 			if len(a) == MAX_ITEMS {
 				l.Error = errors.New("Line too long.")
